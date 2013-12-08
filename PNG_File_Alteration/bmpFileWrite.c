39ae0e976c7697d7853e1cd5c1672e610b4f67c0
William Fong <trigition@gmail.com>
2013-12-08 00:37:33 -0700

    Further revised bmpBotton
    Added comments, cleaned code, etc.

--- PNG_File_Alteration/bmpFileWrite.c
+++ PNG_File_Alteration/bmpFileWrite.c
@@ -19,24 +19,41 @@
 FILE *bmpBottom(ds_bmp sentinel) {
     FILE *newFile = NULL;
     char *fileName = NULL;
+    
+    
+    //Check for what variables need to be defaulted
     if(get_name(sentinel) == NULL)
     {
         fileName = defaultFileNameWrite();
+        if (fileName == NULL) {
+            printf("ERROR: Default filename creation failed\n");
+        }
     }
     else
     {
         fileName = get_name(sentinel);
     }
+    
+    
     char *data = NULL;
     unsigned long estSize = get_width(sentinel) * get_height(sentinel);
     unsigned long i;
+    
     newFile = fopen(fileName, "w");
+    
+    //Generate data array to write to file
     data = generateFileHeader(estSize, get_height(sentinel), get_width(sentinel), (char***)get_pixels(sentinel));
+    
+    //WRITE TO FILE
     for (i = 0; data[i] != '\n'; i++) {
         fprintf(newFile, "%c", data[i]);
     }
+    
+    //Free variables which have already been written to file
     free(fileName);
     free(data);
+    
+    //Return file pointer
     return newFile;
 }
 
@@ -67,6 +84,7 @@ char *defaultFileNameWrite() {
 
 
 char *generateFileHeader(unsigned long arraySize, unsigned long height, unsigned long width, char ***pixels) {
+    
     unsigned int i;
     char *header = NULL;
     unsigned long fileSize = 54 + pow(2, BPP/8) + arraySize*3;
